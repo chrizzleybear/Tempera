60e8b75e590db0d85e935b00a59c58568be7fa52
christopherstief <christopher.stief@student.uibk.ac.at>
2024-03-06 17:52:23 +0100

    upload skeleton

---
+++ src/main/java/at/qe/skeleton/configs/WebSecurityConfig.java
@@ -0,0 +1,94 @@
+package at.qe.skeleton.configs;
+
+import javax.sql.DataSource;
+
+import at.qe.skeleton.model.UserxRole;
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+/**
+ * Spring configuration for web security.
+ *
+ * This class is part of the skeleton project provided for students of the
+ * course "Software Engineering" offered by Innsbruck University.
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    private static final String ADMIN = UserxRole.ADMIN.name();
+    private static final String MANAGER = UserxRole.MANAGER.name();
+    private static final String EMPLOYEE = UserxRole.EMPLOYEE.name();
+    private static final String LOGIN = "/login.xhtml";
+    private static final String ACCESSDENIED = "/error/access_denied.xhtml";
+    
+    @Autowired
+    DataSource dataSource;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        
+        try {   
+
+            http
+            .cors(cors -> cors.disable())
+            .csrf(csrf -> csrf.disable())
+            .headers(headers -> headers.frameOptions(FrameOptionsConfig::sameOrigin)) // needed for H2 console
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(new AntPathRequestMatcher("/")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/**.jsf")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/jakarta.faces.resource/**")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/error/**")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/admin/**")).hasAnyAuthority("ADMIN")
+                .requestMatchers(new AntPathRequestMatcher("/secured/**")).hasAnyAuthority(ADMIN, MANAGER, EMPLOYEE)
+                .anyRequest().authenticated()
+            )
+            // :TODO: user failureUrl(/login.xhtml?error) and make sure that a corresponding message is displayed
+            .formLogin(form -> form
+                .loginPage(LOGIN)
+                .permitAll()
+                .defaultSuccessUrl("/secured/welcome.xhtml")
+                .loginProcessingUrl("/login")
+                .successForwardUrl("/secured/welcome.xhtml")
+            )
+            .logout(logout -> logout
+                .logoutSuccessUrl(LOGIN)
+                .deleteCookies("JSESSIONID")
+                .invalidateHttpSession(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+            )
+            .sessionManagement(session -> session
+                .invalidSessionUrl("/error/invalid_session.xhtml")
+            );
+
+            return http.build();
+        } catch (Exception ex) {
+            throw new BeanCreationException("Wrong spring security configuration", ex);
+        }
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        //Configure roles and passwords via datasource
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery("select username, password, enabled from userx where username=?")
+                .authoritiesByUsernameQuery("select userx_username, roles from userx_userx_role where userx_username=?");
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        // :TODO: use proper passwordEncoder and do not store passwords in plain text
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
---
+++ src/main/java/at/qe/skeleton/model/Userx.java
@@ -0,0 +1,201 @@
+package at.qe.skeleton.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.Set;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import org.springframework.data.domain.Persistable;
+
+/**
+ * Entity representing users.
+ *
+ * This class is part of the skeleton project provided for students of the
+ * course "Software Engineering" offered by the University of Innsbruck.
+ */
+@Entity
+public class Userx implements Persistable<String>, Serializable, Comparable<Userx> {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(length = 100)
+    private String username;
+
+    @ManyToOne(optional = false)
+    private Userx createUser;
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createDate;
+    @ManyToOne(optional = true)
+    private Userx updateUser;
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime updateDate;
+
+    private String password;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+
+    boolean enabled;
+
+    @ElementCollection(targetClass = UserxRole.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "Userx_UserxRole")
+    @Enumerated(EnumType.STRING)
+    private Set<UserxRole> roles;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Set<UserxRole> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<UserxRole> roles) {
+        this.roles = roles;
+    }
+
+    public Userx getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(Userx createUser) {
+        this.createUser = createUser;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    public Userx getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(Userx updateUser) {
+        this.updateUser = updateUser;
+    }
+
+    public LocalDateTime getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(LocalDateTime updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + Objects.hashCode(this.username);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Userx)) {
+            return false;
+        }
+        final Userx other = (Userx) obj;
+        if (!Objects.equals(this.username, other.username)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "at.qe.skeleton.model.User[ id=" + username + " ]";
+    }
+
+    @Override
+    public String getId() {
+        return getUsername();
+    }
+
+    public void setId(String id) {
+        setUsername(id);
+    }
+
+    @Override
+    public boolean isNew() {
+        return (null == createDate);
+    }
+
+	@Override
+	public int compareTo(Userx o) {
+		return this.username.compareTo(o.getUsername());
+	}
+
+}
