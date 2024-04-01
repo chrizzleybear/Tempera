8c4cc816fc24acd060b434ab3ce8393c2e01c99d
Leonardo Pedri <leonardo.pedri@uibk.ac.at>
2024-04-01 15:58:34 +0200

    save device data to db shell

--- tempera-accesspoint/tempera/bleclient/device_notify.py
+++ tempera-accesspoint/tempera/bleclient/device_notify.py
@@ -1,33 +1,41 @@
-# https://github.com/hbldh/bleak
-# https://github.com/hbldh/bleak/blob/master/examples/service_explorer.py
-
-import asyncio
-import sys
+import datetime
+import struct
 
 from bleak import BleakClient
 
-device_address = "5A:09:EC:F3:F8:BB"
-battery_notify_uuid = "00002a19-0000-1000-8000-00805f9b34fb"
+from utils.db_utils import save_measurement
 
 
-# data is a byte array
-def battery_notify_handler(characteristic, data):
-    print(f"Battery level is: {int.from_bytes(data, byteorder='little')}")
+def battery_notify_handler(_characteristic, data: bytes) -> None:
+    save_measurement(
+        measurement_id=None,
+        sensor_id=0,
+        value=struct.unpack("<f", data)[0],
+        timestamp=datetime.datetime.now(),
+    )
 
 
-async def main():
-    async with BleakClient(device_address) as client:
-        print(f"Connected to device {device_address}")
-        while True:
-            try:
-                await asyncio.sleep(3)
-                await client.start_notify(battery_notify_uuid, battery_notify_handler)
-                await asyncio.sleep(10)
-            except KeyboardInterrupt:
-                print(f"INFO: Disconnecting from device {device_address} ...")
-                await client.stop_notify(battery_notify_uuid)
-                print(f"INFO: Disconnected from device {device_address}")
-                sys.exit(0)
+# maps each uuid to its respective handler
+uuid_mapper = {"00002a19-0000-1000-8000-00805f9b34fb": battery_notify_handler}
 
 
-asyncio.run(main())
+async def start_notify(client: BleakClient, uuid: str) -> None:
+    """
+    Starts the notification to the server and maps the uuid to the respective handler
+
+    :param client:
+    :param uuid:
+    :return:
+    """
+    await client.start_notify(uuid, uuid_mapper[uuid])
+
+
+def stop_notify(client: BleakClient) -> None:
+    """
+    Stop all notifiers
+
+    :param client:
+    :return:
+    """
+    for key in uuid_mapper.keys():
+        client.stop_notify(key)
