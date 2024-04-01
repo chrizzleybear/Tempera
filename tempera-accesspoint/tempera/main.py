65cbc2e8d5f0b179f3c14f49552077d661f973a0
Leonardo Pedri <leonardo.pedri@uibk.ac.at>
2024-04-01 22:50:17 +0200

    move and automate device discovery

--- tempera-accesspoint/tempera/main.py
+++ tempera-accesspoint/tempera/main.py
@@ -1,19 +1,18 @@
 import asyncio
 import sys
 
-from bleak import BleakClient
+from bleak import BleakClient, BleakScanner
 
 from api.poster import post
-from bleclient.device_notify import start_notify, stop_notify
-
-device_address = "5A:09:EC:F3:F8:BB"
+from bleclient.device_notify import stop_notify, detection_callback, start_notify
+from utils.db_utils import delete_measurements
 
 uuids = {
-    "BATTERY_LEVEL": "00002a19-0000-1000-8000-00805f9b34fb",
-    "TEMPERATURE": "0x272F",  # in deg C
-    "AIRQUALITY": "",
-    "LIGHTINTENSITY": "",
-    "HUMIDITY": "",
+    # "BATTERY_LEVEL": "00002a19-0000-1000-8000-00805f9b34fb",
+    "TEMPERATURE": "00002a6e-0000-1000-8000-00805f9b34fb",  # in deg C
+    # "AIRQUALITY": "",
+    # "LIGHTINTENSITY": "",
+    # "HUMIDITY": "2A6F",
 }
 
 data_collection_interval = 10
@@ -21,16 +20,38 @@ data_sending_interval = 5
 
 
 async def main():
+    delete_measurements()
+
+    scanner = BleakScanner(detection_callback)
+    print("Scanning for BLE devices...")
+    devices = await scanner.discover(timeout=5)
+
+    devs = []
+    for device in devices:
+        if "G4T1" in device.name:
+            devs.append(device)
+        # TODO: remove for production
+        elif "Mi 9 Lite" in device.name:  # for testing only
+            devs.append(device)
+
+    print(f"Tempera stations found {devs}")
     # TODO: adapt to multiple connected devices
+    device_address = devs[0].address  # for now only care about handling 1 device
+
     async with BleakClient(
         device_address,
         timeout=120,
-        services=list(uuids.values()),
+        # services=list(uuids.values()),
         disconnected_callback=stop_notify,
     ) as client:
         print(f"Connected to device {device_address}.")
 
         while True:
+            for service in client.services:
+                print(f"Service: {service}")
+                for characteristic in service.characteristics:
+                    print(f"Characteristics: {characteristic}")
+
             for val in uuids.values():
                 await start_notify(client, val)
 
