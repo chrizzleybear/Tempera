33b7ce98f143ece433fbb3c15093e5ae65b9d495
csaw6568 <simon.c.froehlich@student.uibk.ac.at>
2024-04-05 14:57:08 +0200

    implemented basic connectivity and service to transmit time data

--- tempera-station/src/main.cpp
+++ tempera-station/src/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 #include <Arduino.h>
 #include <ArduinoBLE.h>
@@ -9,11 +8,12 @@
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BME680.h"
 
+#include <stdint.h>
 
 
 
 
-// ############### MAKROS ############### 
+// ############### PARAMETERS ############### 
 
 // Toggle additional Informations in serial output:
 #define INFO 1
@@ -40,6 +40,10 @@
 
 // Delay after which a new button press will be accepted
 #define BUTTON_COOLDOWN 500
+
+// Set the device name and custom id
+#define DEVICE_NAME "Tempera-Station #1"
+#define DEVICE_ID "1234567890"
 
 
 
@@ -112,19 +116,40 @@ class LED {
     }
 };
 
+struct uint48 {
+    uint64_t x:48;
+} __attribute__((packed));
+
 
 
 
 
 // ############### BLE SETUP ###############
 
+// Setup the device information service
+BLEService deviceInformationService("180A");
+BLEStringCharacteristic serialNumberCharacteristic("2A25", BLERead, 32);
+BLEStringCharacteristic manufacturerNameCharacteristic("2A29", BLERead, 64);
 
+// Setup the elapsed time service for the work time tracking
+struct elapsedTimeCharacteristicStructure {
+  uint8_t flags;
+  uint32_t timeValue;
+  uint8_t timeSyncSource;
+  uint8_t offset;
+};
+BLEService elapsedTimeService("183F");
+BLECharacteristic currentElapsedTimeCharacteristic("2BF2", BLERead, 9);
 
+// Functions for connection and data transfer
+void blePeripheralConnectHandler(BLEDevice central);
+void blePeripheralDisconnectHandler(BLEDevice central);
 
+void readManufacturerName(BLEDevice central, BLECharacteristic characteristic);
+void readElapsedTime(BLEDevice central, BLECharacteristic characteristic);
+void readSerialNumber(BLEDevice central, BLECharacteristic characteristic);
 
-
-
-
+void writeElapsedTimeCharacteristicStructure(elapsedTimeCharacteristicStructure structure);
 
 
 // ############### SETUP CODE ###############
@@ -132,6 +157,8 @@ class LED {
 // Create objects
 LED led;
 timedSession session;
+elapsedTimeCharacteristicStructure currentElapsedTime = {0, 0, 0, 0};
+
 
 void setup() {
   // Setup for the rgb-led pins
@@ -147,14 +174,57 @@ void setup() {
 
   // Set serial output data rate in bits/s
   Serial.begin(SERIAL_DATA_RATE);
+  if (INFO) Serial.println("Tempera > [INFO] Serial started");
 
   // Set to Out-Of-Office when device is started
   session.workMode = 4;
   session.startTime = millis();
   session.lastSessionDuration = 0;
-
   led.setColor(findButtonColor(session.workMode));
   led.toggleLED();
+
+  // BLE: Setup for BLE connectivity and device infos
+  if (INFO) {
+    if (!BLE.begin()) {
+      Serial.println("Tempera > [ERROR] Starting BLE failed!");
+      while(1);
+    }
+    Serial.println("Tempera > [INFO] BLE startet.");
+  }
+  BLE.setEventHandler(BLEConnected, blePeripheralConnectHandler);
+  BLE.setEventHandler(BLEDisconnected, blePeripheralDisconnectHandler);
+  BLE.setLocalName(DEVICE_NAME);
+  BLE.setDeviceName(DEVICE_NAME);
+  BLE.setAdvertisedService(deviceInformationService);
+
+  // BLE: Setup for user description and system ID
+  BLEDescriptor deviceInfoDescriptor("2901", "Sensor station with group number");
+  manufacturerNameCharacteristic.addDescriptor(deviceInfoDescriptor);
+  manufacturerNameCharacteristic.writeValue("G4T1");
+  manufacturerNameCharacteristic.setEventHandler(BLERead, readManufacturerName);
+  deviceInformationService.addCharacteristic(manufacturerNameCharacteristic);
+
+  // to-do: serial number is not correctly transmitted? 
+  // only the first characteristic can be accessed
+  BLEDescriptor serialInfoDescriptor("2901", "Unique serial number");
+  serialNumberCharacteristic.addDescriptor(serialInfoDescriptor);
+  serialNumberCharacteristic.writeValue(DEVICE_ID);
+  serialNumberCharacteristic.setEventHandler(BLERead, readSerialNumber);
+  deviceInformationService.addCharacteristic(serialNumberCharacteristic);
+
+  BLE.addService(deviceInformationService);
+
+  // BLE: Setup for elapsed time characteristic
+  BLEDescriptor elapsedTimeDescriptor("2901", "Elapsed Time service used for time tracking.");
+  currentElapsedTimeCharacteristic.addDescriptor(elapsedTimeDescriptor);
+  currentElapsedTimeCharacteristic.setEventHandler(BLERead, readElapsedTime);
+  elapsedTimeService.addCharacteristic(currentElapsedTimeCharacteristic);
+  BLE.addService(elapsedTimeService);
+  elapsedTimeCharacteristicStructure initialValue = {0, 0, 0, 0};
+  writeElapsedTimeCharacteristicStructure(initialValue);
+
+  // BLE: Advertise services
+  BLE.advertise();
 }
 
 
@@ -183,8 +253,16 @@ void loop() {
     delay(BUTTON_COOLDOWN);
   }
 
+
+  // Test
+  currentElapsedTimeCharacteristic.writeValue((uint8_t *)&currentElapsedTime, sizeof(currentElapsedTime));
+  
+
+  // Check for events
+  BLE.poll();
+
   // Wait for a bit
-  delay(300);
+  delay(1000);
 }
 
 
@@ -254,266 +332,39 @@ void printLEDUpdate() {
   Serial.println(led.color.blue);
   return;
 }
-=======
 
-#include <Arduino.h>
-#include <ArduinoBLE.h>
-
-// Libraries for the BME sensor module
-#include <Wire.h>
-#include <SPI.h>
-#include <Adafruit_Sensor.h>
-#include "Adafruit_BME680.h"
-
-
-
-
-
-// ############### MAKROS ############### 
-
-// Toggle additional Informations in serial output:
-#define INFO 1
-
-// Define Output Pins for LED
-#define LED_R A0
-#define LED_G A1
-#define LED_B A2
-
-// Define aliases for the button modes
-// Order: Deep-Work, Meeting, Out-Of-Office, Anwesend
-#define B_DW D2 
-#define B_MT D3
-#define B_OO D4
-#define B_AW D5
-// Set their colors (rgb value):
-#define B_DW_COLOR {0, 0, 255}
-#define B_MT_COLOR {255, 40, 10}
-#define B_OO_COLOR {255, 0, 0}
-#define B_AW_COLOR {0, 64, 0}
-
-// Serial data rate in bits/s
-#define SERIAL_DATA_RATE 9600
-
-// Delay after which a new button press will be accepted
-#define BUTTON_COOLDOWN 500
-
-
-
-
-
-// ############### FUNCTIONS DECLARATIONS ############### 
-
-pin_size_t whichButtonPressed();
-struct color findButtonColor(pin_size_t button);
-void printSessionUpdate();
-void printLEDUpdate();
-
-
-
-
-
-// ############### CLASS AND STRUCT DECLARATIONS ############### 
-// Attributes are public to skip Getter and Setter.
-
-struct color {
-  int red;
-  int green;
-  int blue;
-};
-
-class timedSession {
-  public:
-    int active = 0;
-    int workMode = 0;
-    unsigned long startTime = millis();
-    unsigned long lastSessionDuration = 0;
-
-    int toggleActive () {
-      this->active = !this->active;
-      return this->active;
-    }
-};
-
-class LED {
-  public:
-    int ledStatus = 0;
-    struct color color = {0, 0, 0};
-
-    void toggleLED() {
-      this->ledStatus = !this->ledStatus;
-      this->ledStatus ? turnOn() : turnOff();
-      return;
-    }
-
-    void turnOn() {
-      this->ledStatus = 1;
-      analogWrite(LED_R, color.red);
-      analogWrite(LED_G, color.green);
-      analogWrite(LED_B, color.blue);
-      return;
-    }
-
-    void turnOff() {
-      setColor({0, 0, 0});
-      turnOn();
-      this->ledStatus = 0;
-      return;
-    }
-
-    void setColor(struct color color) {
-      this->color.red = color.red;
-      this->color.green = color.green;
-      this->color.blue = color.blue;
-      return;
-    }
-};
-
-
-
-
-
-// ############### BLE SETUP ###############
-
-
-
-
-
-
-
-
-
-
-// ############### SETUP CODE ###############
-
-void setup() {
-  // Setup for the rgb-led pins
-  pinMode(LED_R, OUTPUT);
-  pinMode(LED_G, OUTPUT);
-  pinMode(LED_B, OUTPUT);
-
-  // Setup for the button pins
-  pinMode(B_DW, INPUT_PULLUP);
-  pinMode(B_MT, INPUT_PULLUP);
-  pinMode(B_OO, INPUT_PULLUP);
-  pinMode(B_AW, INPUT_PULLUP);
-
-  // Set serial output data rate in bits/s
-  Serial.begin(SERIAL_DATA_RATE);
-
-  Serial.println("Tempera > [INFO] System started...");
+void blePeripheralConnectHandler(BLEDevice central) {
+  Serial.println("Tempera > [INFO] Connected event, central: ");
+  Serial.print("Tempera > [INFO]    ");
+  Serial.println(central.address());
 }
 
-// Create objects
-LED led;
-timedSession session;
-
-
-
-
-
-// ############### RUNTIME CODE ###############
-
-void loop() {
-
-  // If a button has been pressed change the following:
-  if (pin_size_t b = whichButtonPressed()) {
-
-    // Update the work session info so the duration and time etc since the last mode change
-    if (session.toggleActive()) {
-      session.workMode = b;
-      session.startTime = millis();
-      session.lastSessionDuration = 0;
-    } else {
-      session.workMode = 0;
-      session.lastSessionDuration = millis() - session.startTime;
-      
-      // check for overflow, if yes correct the time
-      // an overflow will happen after leaving the device on for than approximately 50 consecutive days
-      if (session.lastSessionDuration < 0) {
-        
-      }
-      
-      session.startTime = 0;
-    }
-    if (INFO) printSessionUpdate();
-    
-    // Update LED status and print
-    led.setColor(findButtonColor(b));
-    led.toggleLED();
-    if (INFO) printLEDUpdate();
-    
-    delay(BUTTON_COOLDOWN);
-  }
-
-  // Wait for a bit
-  delay(300);
+void blePeripheralDisconnectHandler(BLEDevice central) {
+  Serial.println("Tempera > [INFO] Disconnected event, central: ");
+  Serial.print("Tempera > [INFO]    ");
+  Serial.println(central.address());
 }
 
-
-
-
-
-// ############### FUNCTIONS ###############
-
-// Checks if one of the four buttons is currently pressed and returns the pressed one or 0 
-pin_size_t whichButtonPressed() {
-  if (!digitalRead(B_DW)) {
-    return B_DW;
-  } else if (!digitalRead(B_MT)) {
-    return B_MT;
-  } else if (!digitalRead(B_OO)) {
-    return B_OO;
-  } else if (!digitalRead(B_AW)) {
-    return B_AW;
-  } else {
-    return 0;
-  }
+void readManufacturerName(BLEDevice central, BLECharacteristic characteristic) {
+  Serial.println("Tempera > [INFO] Manufacturer Specifications have been read: ");
+  Serial.print("Tempera > [INFO]    ");
+  Serial.println(central.address()); 
 }
 
-// Returns the corresponding color for each button
-struct color findButtonColor(pin_size_t button) {
-  switch (button) {
-  case B_AW: 
-    return B_AW_COLOR;
-    break;
-  case B_DW:
-    return B_DW_COLOR;
-    break;
-  case B_MT:
-    return B_MT_COLOR;
-    break;
-  case B_OO:
-    return B_OO_COLOR;
-    break;
-  default:
-    return {0, 0, 0};
-    break;
-  }
+void readElapsedTime(BLEDevice central, BLECharacteristic characteristic) {
+  Serial.println("Tempera > [INFO] Elapsed time in current workmode: ");
+  Serial.print("Tempera > [INFO]    ");
+  Serial.println(central.address()); // to-do correct value
 }
 
-void printSessionUpdate() {
-  Serial.println("Tempera > [INFO] Session Info has been updated:");
-  Serial.print("Tempera > [INFO]    Active: ");
-  Serial.println(session.active);
-  Serial.print("Tempera > [INFO]    Current work mode: ");
-  Serial.println(session.workMode);
-  Serial.print("Tempera > [INFO]    Start time: ");
-  Serial.println(session.startTime);
-  Serial.print("Tempera > [INFO]    Last session duration: ");
-  Serial.println(session.lastSessionDuration);
-  return;
+void readSerialNumber(BLEDevice central, BLECharacteristic characteristic) {
+  Serial.println("Tempera > [INFO] SystemID has been read.");
+  // to-do insert system id
 }
 
-void printLEDUpdate() {
-  Serial.println("Tempera > [INFO] LED state has been updated:");
-  Serial.print("Tempera > [INFO]    Active: ");
-  Serial.println(led.ledStatus);
-  Serial.print("Tempera > [INFO]    Color (R-G-B): ");
-  Serial.print(led.color.red);
-  Serial.print(" ");
-  Serial.print(led.color.green);
-  Serial.print(" ");
-  Serial.println(led.color.blue);
-  return;
+void writeElapsedTimeCharacteristicStructure(elapsedTimeCharacteristicStructure structure) {
+  currentElapsedTimeCharacteristic.writeValue(structure.flags);
+  currentElapsedTimeCharacteristic.writeValue(structure.timeValue);
+  currentElapsedTimeCharacteristic.writeValue(structure.timeSyncSource);
+  currentElapsedTimeCharacteristic.writeValue(structure.offset);
 }
->>>>>>> 460e13d5e68f317ba29fdb11fa546752f7b5f707
