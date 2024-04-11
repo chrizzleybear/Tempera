b231c34bdd553c54268d5322c27e1a9765e3be4b
Andreas Enk <andreas.enk@student.uibk.ac.at>
2024-04-11 16:48:19 +0200

    use prime template for header and sidebar

--- webapp/src/app/app.routes.ts
+++ webapp/src/app/app.routes.ts
@@ -6,14 +6,18 @@ import { BoardModeratorComponent } from './board-moderator/board-moderator.compo
 import { BoardUserComponent } from './board-user/board-user.component';
 import { BoardAdminComponent } from './board-admin/board-admin.component';
 import { HomeComponent } from './home/home.component';
+import { AppLayoutComponent } from './layout/app.layout.component';
 
 export const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'user', component: BoardUserComponent },
-  { path: 'mod', component: BoardModeratorComponent },
-  { path: 'admin', component: BoardAdminComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' }
+  {
+    path: '', component: AppLayoutComponent
+  }
+  // { path: 'home', component: HomeComponent },
+  // { path: 'login', component: LoginComponent },
+  // { path: 'register', component: RegisterComponent },
+  // { path: 'profile', component: ProfileComponent },
+  // { path: 'user', component: BoardUserComponent },
+  // { path: 'mod', component: BoardModeratorComponent },
+  // { path: 'admin', component: BoardAdminComponent },
+  // { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
