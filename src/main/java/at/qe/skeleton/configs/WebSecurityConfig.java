9baa0b93564c33dac3a5100ace69185bf6ecf1a9
christopherstief <christopher.stief@student.uibk.ac.at>
2024-04-12 16:38:36 +0200

    added postgres to dependencies

--- src/main/java/at/qe/skeleton/configs/WebSecurityConfig.java
+++ src/main/java/at/qe/skeleton/configs/WebSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -75,6 +76,9 @@ public class WebSecurityConfig {
                     .headers(
                             headers ->
                                     headers.frameOptions(FrameOptionsConfig::sameOrigin)) // needed for H2 console
+                    .authorizeHttpRequests(authorize -> authorize
+                            .requestMatchers(new AntPathRequestMatcher("/api/**"))
+                            .authenticated()).httpBasic(Customizer.withDefaults())
                     .authorizeHttpRequests(
                             authorize ->
                                     authorize
