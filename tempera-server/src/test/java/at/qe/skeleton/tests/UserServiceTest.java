7a0a4403ac02b990e6de56d54768124153875698
Andreas Enk <andreas.enk@student.uibk.ac.at>
2024-04-19 01:01:16 +0200

    fix UserServiceTest

--- tempera-server/src/test/java/at/qe/skeleton/tests/UserServiceTest.java
+++ tempera-server/src/test/java/at/qe/skeleton/tests/UserServiceTest.java
@@ -13,6 +13,8 @@ import at.qe.skeleton.model.Userx;
 import at.qe.skeleton.model.enums.UserxRole;
 import at.qe.skeleton.services.UserService;
 
+import java.util.HashSet;
+
 /**
  * Some very basic tests for {@link UserService}.
  *
@@ -133,7 +135,9 @@ public class UserServiceTest {
         Userx freshlyCreatedUser = userService.loadUser(username);
         Assertions.assertNotNull(freshlyCreatedUser, "New user could not be loaded from test data source after being saved");
         Assertions.assertEquals(username, freshlyCreatedUser.getUsername(), "New user could not be loaded from test data source after being saved");
-        Assertions.assertEquals(password, freshlyCreatedUser.getPassword(), "User \"" + username + "\" does not have a the correct password attribute stored being saved");
+        // compare the saved password (encrypted), not the password field (plain text) with the user
+        // service retrieved (also encrypted) password
+        Assertions.assertEquals(toBeCreatedUser.getPassword(), freshlyCreatedUser.getPassword(), "User \"" + username + "\" does not have a the correct password attribute stored being saved");
         Assertions.assertEquals(fName, freshlyCreatedUser.getFirstName(), "User \"" + username + "\" does not have a the correct firstName attribute stored being saved");
         Assertions.assertEquals(lName, freshlyCreatedUser.getLastName(), "User \"" + username + "\" does not have a the correct lastName attribute stored being saved");
         Assertions.assertEquals(email, freshlyCreatedUser.getEmail(), "User \"" + username + "\" does not have a the correct email attribute stored being saved");
@@ -153,6 +157,9 @@ public class UserServiceTest {
             Assertions.assertNotNull(adminUser, "Admin user could not be loaded from test data source");
 
             Userx toBeCreatedUser = new Userx();
+
+            // set roles to avoid another exception being thrown before
+            toBeCreatedUser.setRoles(new HashSet<>(Sets.newSet(UserxRole.ADMIN)));
             userService.saveUser(toBeCreatedUser);
         });
     }
