4131ea21ff26793fa00bfd5fb0973dc0a93f7860
Leonardo Pedri <leonardopedri1.0@gmail.com>
2024-04-22 10:05:43 +0200

    change hard-coded params to config file params
    also some light bug-fixing

--- tempera-accesspoint/tempera/bleclient/device_connection.py
+++ tempera-accesspoint/tempera/bleclient/device_connection.py
@@ -1,21 +1,17 @@
-import json
 import logging
-from pathlib import Path
 from typing import List
 
 import requests
-import yaml
 from bleak import BLEDevice, BleakScanner, BleakClient
-from fastapi import requests
 from requests import Response
-from tenacity import retry, wait_fixed
 
 from bleclient.device_notification import detection_callback
+from utils.config_utils import init_config
 
 logger = logging.getLogger(f"tempera.{__name__}")
 
 
-API_ENDPOINT = "http://127.0.0.1:8000"
+CONFIG = init_config()
 REQUIRED_SERVICES = ["180a", "183f", "181a"]
 REQUIRED_CHARACTERISTICS = ["2a29", "2a25", "2bf2", "2a6e", "2a77", "2a6f", "2bd3"]
 SCANNING_TIMEOUT = 5
@@ -59,23 +55,20 @@ async def validate_stations(
     """
     logger.info(f"Trying to validate stations: {tempera_stations}")
 
-    config_file = Path(__name__).resolve().parent.parent / "conf.yaml"
-    if not config_file.is_file():
-        logger.critical(
-            f"{config_file.absolute()} not found. Couldn't get web app server IP, aborting validation."
-        )
-        raise FileNotFoundError
-    logger.debug(f"Reading config file ({config_file.absolute()})")
-
-    with open(config_file, "r") as config_file:
-        config = yaml.safe_load(config_file)
+    try:
+        server_address = CONFIG["webserver_address"]
+        access_point_id = CONFIG["access_point_id"]
+    except KeyError as e:
+        logger.critical(f"Failed to read parameter from the config file: {e}")
+        raise KeyError
 
     response = requests.get(
-        f"{API_ENDPOINT}/rasp/api/{config['accesspoint_id']}",
+        f"{server_address}/rasp/api/valid_devices",
+        params={"device_id": access_point_id},
         hooks={"response": validate_access_point},
     )
 
-    response = json.loads(response.json())
+    response = response.json()
     allowed_stations = response["stations_allowed"]
 
     is_valid = True
@@ -122,7 +115,7 @@ def _validate_station(station: BLEDevice, client: BleakClient) -> bool:
 # Keep scanning for stations every 60 seconds until one is found.
 # Usually a stop after n attempts would probably be better, but with headless raspberry pi
 # this strategy might be preferable
-@retry(wait=wait_fixed(60))
+# @retry(wait=wait_fixed(60))
 async def discovery_loop(check_characteristics=False) -> BLEDevice:
     tempera_stations = await get_tempera_stations()
     if not tempera_stations:
@@ -138,18 +131,28 @@ async def discovery_loop(check_characteristics=False) -> BLEDevice:
     return tempera_station
 
 
-def validate_access_point(response: Response) -> None:
+def validate_access_point(response: Response, *args, **kwargs) -> None:
     code = response.status_code
     response = response.json()
 
     if code != 200:
-        logger.debug(f"{response.status_code}, {response}")
+        logger.debug(f"{code}, {response}")
         raise RuntimeError("An error occurred in calling the API")
-    elif response["access_point_allowed"] != "True":
+    elif not response["access_point_allowed"]:
         logger.debug(f"{response.status_code}, {response}")
         raise RuntimeError("This access point is not registered in the web app server.")
 
 
 def get_scan_order() -> bool:
-    response = requests.get(f"{API_ENDPOINT}/rasp/api/scan_order")
+    try:
+        server_address = CONFIG["webserver_address"]
+    except KeyError as e:
+        logger.critical(f"Failed to read parameter from the config file: {e}")
+        raise KeyError
+
+    response = requests.get(f"{server_address}/rasp/api/scan_order")
+    if response.status_code != 200:
+        logger.debug(f"{response.status_code}, {response.json()}")
+        raise RuntimeError("An error occurred in calling the API")
+
     return response.json()["scan"]
