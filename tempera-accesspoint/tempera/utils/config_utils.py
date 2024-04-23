ad98429939dd2b9b09ea87118881a01e20ea2d63
Leonardo Pedri <leonardopedri1.0@gmail.com>
2024-04-23 15:26:49 +0200

    utils function to create header from config

--- tempera-accesspoint/tempera/utils/config_utils.py
+++ tempera-accesspoint/tempera/utils/config_utils.py
@@ -1,8 +1,9 @@
 import logging
 from pathlib import Path
-from typing import Dict
+from typing import Dict, Any
 
 import yaml
+from requests.auth import HTTPBasicAuth
 
 logger = logging.getLogger(f"tempera.{__name__}")
 
@@ -20,3 +21,21 @@ def init_config() -> Dict | None:
         config = yaml.safe_load(config_file)
 
     return config
+
+
+def init_header(config: Dict[str, Any]) -> HTTPBasicAuth:
+    """
+    Returns the authentication objet to set as the 'headers' kwarg in any request to the web app server.
+    Raises an error if it fails the generation of the authentication object fails.
+
+    :param config:
+    :return:
+    """
+    try:
+        return HTTPBasicAuth(config["user_name"], config["password"])
+    except KeyError as e:
+        logger.critical(
+            "User name or password not found in config file. Unable to authenticate api requests."
+        )
+        logger.exception(e)
+        raise RuntimeError
