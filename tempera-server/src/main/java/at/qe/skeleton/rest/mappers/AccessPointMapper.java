ec942da39443789edb3f60f3f3cb1241b0e3f2c6
christopherstief <christopher.stief@student.uibk.ac.at>
2024-04-25 17:44:19 +0200

    introduced DTOMapperStateless for oneway mapping. AccessPointMapper implements given Interface.

--- tempera-server/src/main/java/at/qe/skeleton/rest/mappers/AccessPointMapper.java
+++ tempera-server/src/main/java/at/qe/skeleton/rest/mappers/AccessPointMapper.java
@@ -2,35 +2,35 @@ package at.qe.skeleton.rest.mappers;
 
 import at.qe.skeleton.exceptions.CouldNotFindEntityException;
 import at.qe.skeleton.model.AccessPoint;
+import at.qe.skeleton.model.TemperaStation;
 import at.qe.skeleton.rest.dtos.AccessPointDto;
 import at.qe.skeleton.services.AccessPointService;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
-public class AccessPointMapper implements DTOMapper<AccessPoint, AccessPointDto> {
+public class AccessPointMapper implements DTOMapperStateless<AccessPoint, AccessPointDto> {
 
-    private final AccessPointService accessPointService;
-
-    public AccessPointMapper(AccessPointService accessPointService) {
-        this.accessPointService = accessPointService;
+  @Override
+  public AccessPointDto mapToDto(AccessPoint entity) throws CouldNotFindEntityException {
+    if (entity == null) {
+      throw new IllegalArgumentException(
+          "Cannot map AccessPoint to AccessPointDto, because AccessPoint is null.");
+    }
+    if (entity.getId() == null || entity.getId().toString().isEmpty()) {
+      throw new CouldNotFindEntityException(
+          "Cannot map AccessPoint to AccessPointDto, because AccessPoint id is null or empty.");
+    }
+    if (entity.getTemperaStations() == null) {
+      throw new CouldNotFindEntityException(
+          "Cannot map AccessPoint to AccessPointDto, because TemperaStations are null.");
     }
 
-    @Override
-    public AccessPointDto mapToDto(AccessPoint entity) throws CouldNotFindEntityException {
-        if(entity == null) {
-            throw new IllegalArgumentException("Cannot map AccessPoint to AccessPointDto, because AccessPoint is null.");
-        }
-        if(entity.getId() == null) {
-            throw new CouldNotFindEntityException("Cannot map AccessPoint to AccessPointDto, because AccessPoint id is null.");
-        }
+    return new AccessPointDto(
+        entity.getId(), entity.isEnabled(), entity.getTemperaStations().stream().toList());
+  }
 
-        accessPointService.getAccessPointById()
-        return null;
-    }
-
-    @Override
-    public AccessPoint mapFromDto(AccessPointDto dto) throws CouldNotFindEntityException {
-        return null;
-    }
 }
