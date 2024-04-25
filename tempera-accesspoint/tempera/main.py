3c800f88588ca58b251abb4a940a7b57aa81989b
Leonardo Pedri <leonardo.pedri@uibk.ac.at>
2024-04-25 16:31:17 +0200

    bugfix async execution

--- tempera-accesspoint/tempera/main.py
+++ tempera-accesspoint/tempera/main.py
@@ -10,8 +10,9 @@ from bleclient.device_connection import (
     discovery_loop,
     validate_stations,
     get_scan_order,
+    get_station_id,
 )
-from bleclient.etl import elapsed_time_handler
+from bleclient.etl import elapsed_time_handler, filter_uuid, measurements_handler
 
 with open("logging_conf.json", "r") as config:
     logging.config.dictConfig(json.load(config))
@@ -32,27 +33,35 @@ async def start_engine() -> sqlalchemy.Engine:
         )
         raise FileNotFoundError
     logger.info(f"Creating database engine from db file: {database}")
-    return create_engine(f"sqlite:///{database}", echo=True)
+    return create_engine(f"sqlite:///{database}", echo=False)
 
 
-# TODO: add retry
-async def get_data(client: BleakClient, db_engine: sqlalchemy.Engine) -> None:
-    elapsed_time_service = list(
-        filter(lambda service: "183f" in service.uuid, client.services)
-    )[0]
-    elapsed_time_uuid = list(
-        filter(
-            lambda characteristic: "2bf2" in characteristic.uuid,
-            elapsed_time_service.characteristics,
-        )
-    )[0]
+async def get_notifications(client: BleakClient, db_engine: sqlalchemy.Engine) -> None:
+    elapsed_time_service = await filter_uuid(client, "183f")
+    elapsed_time_uuid = await filter_uuid(elapsed_time_service, "2bf2")
 
+    station_id = await get_station_id(client)
     await client.start_notify(
         elapsed_time_uuid,
         elapsed_time_handler,
         engine=db_engine,
-        station_id=client.address,
+        station_id=station_id,
     )
+    logger.info("Subscribed to time record notifications.")
+
+
+async def get_measurements(client: BleakClient, db_engine: sqlalchemy.Engine) -> None:
+    measurement_service = await filter_uuid(client, "181a")
+    characteristics = ["2a6e", "2a77", "2a6f", "2bd3"]
+    logger.info(
+        f"Getting measurements for the following characteristics: {characteristics}."
+    )
+    uuids = []
+    for uuid in characteristics:
+        uuids.append(await filter_uuid(measurement_service, uuid))
+
+    station_id = await get_station_id(client)
+    await measurements_handler(client, uuids, db_engine, station_id)
 
 
 # TODO: add retry
@@ -62,32 +71,39 @@ async def post_data(client: BleakClient) -> None:
 
 # @retry()
 async def main():
-    async with asyncio.TaskGroup() as tg:
-        tempera_station = tg.create_task(discovery_loop())
-        db_engine = tg.create_task(start_engine())
+    db_engine = await start_engine()
+    tempera_station = await discovery_loop(db_engine)
 
-    tempera_station, db_engine = tempera_station.result(), db_engine.result()
-    while True:
-        async with asyncio.TaskGroup() as tg:
-            # Check that the access point and tempera station are still approved by the web app server.
-            valid_station = tg.create_task(validate_stations([tempera_station]))
+    async with BleakClient(tempera_station) as client:
+        logger.debug(f"Connected to device {tempera_station.address}.")
 
-            # If the scan order is issued by the web app server, an error is thrown.
-            # This will cause tenacity to execute the main function from the top again,
-            # starting with device discovery. This is sort of a 'goto'.
-            scan_order = tg.create_task(get_scan_order())
+        await get_notifications(client, db_engine)
 
-            tg.create_task(asyncio.sleep(DATA_COLLECTION_INTERVAL))
+        while True:
+            async with asyncio.TaskGroup() as tg:
+                # Check that the access point and tempera station are still approved by the web app server.
+                valid_station = tg.create_task(
+                    validate_stations(tempera_station, client, db_engine)
+                )
 
-        tempera_station = valid_station.result()
-        if scan_order.result():
-            logger.info("Scan order received. Returning to device discovery.")
-            raise RuntimeError
+                # If the scan order is issued by the web app server, an error is thrown.
+                # This will cause tenacity to execute the main function from the top again,
+                # starting with device discovery. This is sort of a 'goto'.
+                scan_order = tg.create_task(get_scan_order())
 
-        async with BleakClient(tempera_station.address) as client:
-            logger.debug(f"Connected to device {tempera_station.address}.")
+                measurements = tg.create_task(get_measurements(client, db_engine))
 
-            await get_data(client, db_engine)
+                _ = tg.create_task(asyncio.sleep(DATA_COLLECTION_INTERVAL))
+
+            valid_station = valid_station.result()
+            if valid_station != tempera_station:
+                logger.warning(f"{tempera_station} is no longer a valid station.")
+                raise RuntimeError
+            if scan_order.result():
+                logger.info("Scan order received. Returning to device discovery.")
+                raise RuntimeError
+
+            measurements.result()
             await post_data(client)
 
 
