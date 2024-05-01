99ca4990259433e88179ee88fc3c829ea5a50ac3
Leonardo Pedri <leonardo.pedri@uibk.ac.at>
2024-05-01 16:58:10 +0200

    force access point id to be a UUID
    and clean up config script input parsing.

--- tempera-accesspoint/src/configure.py
+++ tempera-accesspoint/src/configure.py
@@ -1,10 +1,11 @@
-import logging
+import time
 from getpass import getpass
 from pathlib import Path
+from typing import Literal
+from uuid import UUID
 
 import yaml
-
-logging.getLogger("configGenerator")
+from tenacity import retry
 
 welcome_art = """
 #######################################################
@@ -23,51 +24,82 @@ welcome_art = """
 """
 
 
-def prompt(message: str, parse_float: bool = False) -> str | float:
-    param = None
-    while not param:
-        param = input(message)
-        if not param:
-            print("---\n❌ No input provided ❌\n➡️  Please provide an input.\n---")
-        elif parse_float:
+@retry()
+def _prompt(
+    message: str, parse: Literal["UUID", "float", "str"] = "str"
+) -> float | str:
+    parameter = input(message)
+
+    if not parameter or parameter == "":
+        print("---\n❌ No input provided ❌\n➡️  Please provide an input.\n---")
+        raise ValueError
+
+    match parse:
+        case "UUID":
             try:
-                return float(param)
+                # Ensure that the input is a valid UUID but return the original string for API validation.
+                # (The UUID __repr__ isn't the same as the input when writing it to file,
+                # the python object represents it as an int or hex which can cause ambiguity
+                # when comparing it to the representation of the web server.)
+                UUID(parameter)
+                return parameter
             except ValueError:
                 print(
-                    "---\n❌ The sending interval must be a number (int or float) ❌\n➡️  Please try again.\n---"
+                    "---\n❌ Provided UUID is not valid. ❌\n➡️  Please try again.\n---"
                 )
-                param = None  # or the loop wouldn't continue
-    return param
+                raise ValueError
+        case "float":
+            try:
+                return float(parameter)
+            except ValueError:
+                print("---\n❌ Expected a number. ❌\n➡️  Please try again.\n---")
+                raise ValueError
+        case _:
+            return parameter
+
+
+@retry()
+def _prompt_password() -> str:
+    parameter = getpass()
+
+    if parameter == "":
+        print(
+            "---\n❌ The password field cannot be empty. ❌\n➡️  Please try again.\n---"
+        )
+        raise ValueError
+
+    return parameter
 
 
 def main():
     print(welcome_art)
 
-    config = {}
     config_file = Path(__file__).parent.resolve() / "conf.yaml"
+    config = dict()
 
     with open(config_file, "w") as cf:
-        config["access_point_id"] = prompt("Set the ID of this access point >> ")
-        config["webserver_address"] = "http://" + prompt(
-            "Set the IP address and port of the web server in the following format: <IP-address>:<port>\n"
-            "http://"
+        config["access_point_id"] = _prompt(
+            "Set the ID of this access point >> ", "UUID"
         )
-        config["user_name"] = prompt(
+
+        config["webserver_address"] = "http://" + _prompt(
+            "Set the IP address and port of the web server in the following format: <IP-address>:<port>\n"
+            "http://",
+        )
+
+        config["user_name"] = _prompt(
             "Set the user name for the web server api authentication >> "
         )
-        config["password"] = ""
-        while config["password"] == "":
-            config["password"] = getpass()
+        config["password"] = _prompt_password()
 
-        config["sending_interval"] = prompt(
-            "Set a data transfer interval in seconds "
-            "between access point and web server >> ",
-            parse_float=True,
+        config["sending_interval"] = _prompt(
+            "Set a data transfer interval in seconds between access point and web server >> ",
+            "float",
         )
+
         yaml.dump(config, cf)
 
     print("\nSetup done! ✨🚀✨\nGood bye. ")
-    import time
 
     time.sleep(0.5)
 
