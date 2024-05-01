2e6de6667189f11e96fc417b0e4ecbd82ef56b61
Leonardo Pedri <leonardo.pedri@uibk.ac.at>
2024-05-01 15:52:04 +0200

    generalize functions for both TimeRecords and Measurements
    reduce duplicate functions with slight changes for both data types into single, flexible functions

--- tempera-accesspoint/src/tempera/api/send.py
+++ tempera-accesspoint/src/tempera/api/send.py
@@ -2,7 +2,6 @@ import asyncio
 import logging
 from typing import Dict, Any, Sequence, Tuple, Literal
 
-import requests
 import sqlalchemy.orm
 from sqlalchemy import select
 from sqlalchemy.orm import Session
@@ -13,33 +12,13 @@ from tempera.utils import shared, make_request
 logger = logging.getLogger(f"tempera.{__name__}")
 
 
-async def send_measurements():
-    with Session(shared.db_engine) as session:
-        measurements, tempera_station = get_from_database(session, "Measurement")
+# Explanation of the func(*, kwarg) notation i.e., how to force kwarg only input.
+# https://www.youtube.com/watch?v=R8-oAqCgHag
 
-        if not measurements:
-            logger.warning(f"No measurements found for station {tempera_station.id}")
-
-        payload = build_measurements_payload(measurements, tempera_station)
-
-        try:
-            logger.info(f"Sending {len(measurements)} measurement(s).")
-            await make_request(
-                "post",
-                f"{shared.config['webserver_address']}/rasp/api/measurements",
-                auth=shared.header,
-                json=payload,
-            )
-        except requests.ConnectionError:
-            logger.error(
-                "Request failed. Couldn't establish a connection to the web server."
-            )
-            raise ConnectionError
-
-        delete_measurements(session, measurements)
+DataType = Literal["TimeRecord", "Measurement"]
 
 
-def get_tempera_station(session: sqlalchemy.orm.Session) -> TemperaStation:
+def _get_tempera_station(session: sqlalchemy.orm.Session) -> TemperaStation:
     tempera_station = session.scalars(
         select(TemperaStation).where(TemperaStation.id == shared.current_station_id)
     ).first()
@@ -53,8 +32,28 @@ def get_tempera_station(session: sqlalchemy.orm.Session) -> TemperaStation:
     return tempera_station
 
 
-def build_measurements_payload(
-    measurements: Sequence[Measurement],
+def _get_from_database(
+    session: sqlalchemy.orm.Session, *, kind: DataType
+) -> Tuple[Sequence[TimeRecord | Measurement], TemperaStation]:
+    tempera_station = _get_tempera_station(session)
+
+    result = None
+    if kind == "TimeRecord":
+        result = session.scalars(
+            select(TimeRecord)
+            .where(TimeRecord.tempera_station_id == shared.current_station_id)
+            .order_by(TimeRecord.start.desc())
+        ).all()
+    elif kind == "Measurement":
+        result = session.scalars(
+            select(Measurement)
+            .where(Measurement.tempera_station_id == shared.current_station_id)
+            .order_by(Measurement.timestamp.desc())
+        ).all()
+
+    return result, tempera_station
+
+
 def _build_payload(
     tempera_station: TemperaStation,
     data: Measurement | TimeRecord,
@@ -85,85 +84,56 @@ def _build_payload(
             }
 
 
+async def send_data(*, kind: DataType):
+    if kind not in ["Measurement", "TimeRecord"]:
+        logger.critical(f"Can't handle data of the {kind} type.")
+        raise ValueError
 
-def delete_measurements(session, measurements: Sequence[Measurement]) -> None:
-    [session.delete(measurement) for measurement in measurements]
-    session.commit()
-
-
-async def send_time_records() -> None:
     with Session(shared.db_engine) as session:
-        time_records, tempera_station = get_from_database(session, "TimeRecord")
+        data, tempera_station = _get_from_database(session, kind=kind)
 
-        payload = build_time_records_payload(time_records, tempera_station)
+        if not data:
+            logger.warning(f"No measurements found for station {tempera_station.id}")
 
-        try:
-            logger.info(f"Sending time {len(time_records)} record(s).")
-            await make_request(
-                "post",
-                f"{shared.config['webserver_address']}/rasp/api/time_records",
-                auth=shared.header,
-                json=payload,
-            )
-        except requests.ConnectionError:
-            logger.error(
-                "Request failed. Couldn't establish a connection to the web server."
-            )
-            raise ConnectionError
+        payloads = [_build_payload(tempera_station, item, kind=kind) for item in data]
 
-        delete_time_records(session, time_records)
+        match kind:
+            case "Measurement":
+                endpoint = "measurements"
+            case "TimeRecord":
+                endpoint = "time_records"
 
+        async with asyncio.TaskGroup() as tg:
+            logger.info(f"Sending {len(data)} {kind}(s).")
+            _ = [
+                tg.create_task(
+                    make_request(
+                        "post",
+                        f"{shared.config['webserver_address']}/rasp/api/{endpoint}",
+                        auth=shared.header,
+                        json=payload,
+                    )
+                )
+                for payload in payloads
+            ]
 
-def get_from_database(
-    session: sqlalchemy.orm.Session, kind: Literal["TimeRecord", "Measurement"]
-) -> Tuple[Sequence[TimeRecord | Measurement], TemperaStation]:
-    tempera_station = get_tempera_station(session)
-
-    result = None
-    if kind == "TimeRecord":
-        result = session.scalars(
-            select(TimeRecord)
-            .where(TimeRecord.tempera_station_id == shared.current_station_id)
-            .order_by(TimeRecord.start.desc())
-        ).all()
-    elif kind == "Measurement":
-        result = session.scalars(
-            select(Measurement)
-            .where(Measurement.tempera_station_id == shared.current_station_id)
-            .order_by(Measurement.timestamp.desc())
-        ).all()
-
-    return result, tempera_station
+        _safe_delete_data(session, data, kind=kind)
 
 
-# TODO: implement api schema and build payload function
-def build_time_records_payload(
-    time_records: Sequence[TimeRecord], tempera_station: TemperaStation
-) -> Dict[str, Any]:
-    return {
-        "access_point_id": shared.config["access_point_id"],
-        "time_records": [
-            {
-                "tempera_station_id": tempera_station.id,
-                "start": f"{time_record.start}",
-                # ms / 1_000 = seconds (cast back to int because division turns the value automatically to float)
-                "duration": int(time_record.duration / 1_000),
-                "mode": time_record.mode,
-                "auto_update": time_record.auto_update,
-            }
-            for time_record in time_records
-        ],
-    }
-
-
-def delete_time_records(
-    session: sqlalchemy.orm.Session, time_records: Sequence[TimeRecord]
+def _safe_delete_data(
+    session, data: Sequence[Measurement | TimeRecord], *, kind: DataType
 ) -> None:
-    [session.delete(time_record) for time_record in time_records]
+    # Remove the time_record with auto_update == True from the list of those to be deleted
+    # so that it keeps being updated at every etl cycle.
+    # ETL should ensure that only the very last time_record can have auto_update == True
+    if kind == "TimeRecord":
+        data = list(filter(lambda time_record: not time_record.auto_update, data))
+
+    [session.delete(item) for item in data]
     session.commit()
 
 
 async def send_measurements_and_time_records():
     async with asyncio.TaskGroup() as tg:
-        _ = tg.create_task(send_measurements())
-        _ = tg.create_task(send_time_records())
+        _ = tg.create_task(send_data(kind="Measurement"))
+        _ = tg.create_task(send_data(kind="TimeRecord"))
