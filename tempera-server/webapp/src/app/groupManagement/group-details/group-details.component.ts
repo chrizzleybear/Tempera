d1d2143441134705fd63b4b61520d02043541ced
Io1211 <iolanthe.hochleitner@student.uibk.ac.at>
2024-05-05 01:23:04 +0200

    Add group details

---
+++ tempera-server/webapp/src/app/groupManagement/group-details/group-details.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import {Group} from "../../models/group.model";
+import {ActivatedRoute} from "@angular/router";
+import {GroupService} from "../../_services/group.service";
+import {CardModule} from "primeng/card";
+import {NgForOf, NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-group-details',
+  standalone: true,
+  imports: [
+    CardModule,
+    NgIf,
+    NgForOf
+  ],
+  templateUrl: './group-details.component.html',
+  styleUrl: './group-details.component.css'
+})
+export class GroupDetailsComponent {
+
+  group: Group | undefined;
+  groupId: string | null | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private groupService: GroupService,
+  ) {
+  }
+
+  ngOnInit() {
+    this.groupId = this.route.snapshot.paramMap.get('id');
+    if (this.groupId) {
+      this.fetchGroupDetails(this.groupId);
+    }
+  }
+
+  fetchGroupDetails(id: string) {
+    this.groupService.getGroupById(id).subscribe({
+      next: (data) => {
+        this.group = data;
+        console.log('Group details: ', this.group);
+      },
+      error: (error) => {
+        console.error('Failed to load group details:', error);
+      },
+    });
+  }
+}
