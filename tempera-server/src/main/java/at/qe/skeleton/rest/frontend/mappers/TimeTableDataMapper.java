9a5d6b526c9f746a4f24d3bc1370186fcce706fd
christopher stief <christopher.stief@student.uibk.ac.at>
2024-05-17 18:32:35 +0200

    timetablemapper

---
+++ tempera-server/src/main/java/at/qe/skeleton/rest/frontend/mappers/TimeTableDataMapper.java
@@ -0,0 +1,18 @@
+package at.qe.skeleton.rest.frontend.mappers;
+
+import at.qe.skeleton.model.Userx;
+import at.qe.skeleton.rest.frontend.dtos.TimetableEntryDto;
+import at.qe.skeleton.rest.frontend.payload.response.GetTimetableDataResponse;
+
+public class TimeTableDataMapper {
+
+
+    public GetTimetableDataResponse getTimetableData(Userx user){
+
+
+        TimetableEntryDto entry = new TimetableEntryDto()
+
+        return null;
+    }
+
+}
