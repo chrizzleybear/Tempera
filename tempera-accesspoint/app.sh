ddcdb5def3ca81d6a2258d115b3469fdf5f11978
Leonardo Pedri <leonardo.pedri@uibk.ac.at>
2024-05-19 11:43:42 +0200

    fix path and control flow issues

--- tempera-accesspoint/app.sh
+++ tempera-accesspoint/app.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-cd src || exit
+cd ./src || exit
 
 OVERWRITE=$1
-if [ "$#" -ne 1 ]; then
-  echo "Warning: parameter OVERWRITE was not provided. Continuing as if OVERWRITE == false."
-  echo "If you would like to configure the tempera access point pass 'true' as a command line argument to docker compose run:"
-  echo "'$ docker compose run ble-app true'"
-  echo "If not, ignore this warning, the existing config file 'config.yaml' will be used."
+if [ ! -f conf.yaml ]; then
+  echo -e "\nNo configuration file 'conf.yaml' found. Launching configuration script.\n\n"
+  sleep 2
+  python3 configure.py;
   python3 main.py
   exit 0
-elif [ ! -f conf.yaml ] || [ "$OVERWRITE" == "true" ]; then
+elif [ "$#" -ne 1 ]; then
+  echo "Warning: this script takes exactly one parameter, but more/less were provided."
+  echo "Continuing as if OVERWRITE == false. If you would like to configure the tempera access point"
+  echo "pass exactly one parameter ('true') as a command line argument to docker compose run:"
+  echo "'$ docker compose run ble-app true'"
+  echo "If not, ignore this warning. The existing config file 'config.yaml' will be used."
+  sleep 5s
+  python3 main.py
+  exit 0
+elif [ "$OVERWRITE" == "true" ]; then
   python3 configure.py;
   python3 main.py
   exit 0
@@ -17,6 +25,7 @@ elif [ "$OVERWRITE" == "false" ]; then
   echo "Note: OVERWRITE == false. Using the existing config file 'config.yaml'."
   echo "If you want to update the configuration pass 'true' as a command line argument to docker compose run:"
   echo "'$ docker compose run ble-app true'"
+  sleep 5s
   python3 main.py
   exit 0
 fi
