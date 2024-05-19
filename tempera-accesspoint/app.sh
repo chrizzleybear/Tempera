c0dd2cf2136420b098b0f3177f8a2ab19846e42c
Leonardo Pedri <leonardo.pedri@uibk.ac.at>
2024-05-19 11:13:50 +0200

    improve python docker caching and configuration

--- tempera-accesspoint/app.sh
+++ tempera-accesspoint/app.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 cd src || exit
 
-# run
-python3 configure.py &&
-python3 main.py
+OVERWRITE=$1
+if [ "$#" -ne 1 ]; then
+  echo "Warning: parameter OVERWRITE was not provided. Continuing as if OVERWRITE == false."
+  echo "If you would like to configure the tempera access point pass 'true' as a command line argument to docker compose run:"
+  echo "'$ docker compose run ble-app true'"
+  echo "If not, ignore this warning, the existing config file 'config.yaml' will be used."
+  python3 main.py
+  exit 0
+elif [ ! -f conf.yaml ] || [ "$OVERWRITE" == "true" ]; then
+  python3 configure.py;
+  python3 main.py
+  exit 0
+elif [ "$OVERWRITE" == "false" ]; then
+  echo "Note: OVERWRITE == false. Using the existing config file 'config.yaml'."
+  echo "If you want to update the configuration pass 'true' as a command line argument to docker compose run:"
+  echo "'$ docker compose run ble-app true'"
+  python3 main.py
+  exit 0
+fi
