399ca545a330df490ef8b8ac8a6b23647d5bf559
Andreas Enk <andreas.enk@student.uibk.ac.at>
2024-05-27 00:32:32 +0200

    add sign out button to topbar / header

--- tempera-server/webapp/src/app/layout/app.topbar.component.ts
+++ tempera-server/webapp/src/app/layout/app.topbar.component.ts
@@ -3,12 +3,15 @@ import { MenuItem } from 'primeng/api';
 import { LayoutService } from './service/app.layout.service';
 import { NgClass } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../_services/auth.service';
+import { StorageService } from '../_services/storage.service';
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-topbar',
   templateUrl: './app.topbar.component.html',
   standalone: true,
-  imports: [RouterLink, NgClass],
+  imports: [RouterLink, NgClass, TooltipModule],
 })
 export class AppTopBarComponent {
 
@@ -20,6 +23,20 @@ export class AppTopBarComponent {
 
   @ViewChild('topbarmenu') menu!: ElementRef;
 
-  constructor(public layoutService: LayoutService) {
+  constructor(public layoutService: LayoutService, private authService: AuthService, private storageService: StorageService) {
+  }
+
+  logout() {
+    this.authService.logout().subscribe({
+      next: res => {
+        console.log(res);
+        this.storageService.clean();
+
+        window.location.reload();
+      },
+      error: err => {
+        console.log(err);
+      },
+    });
   }
 }
