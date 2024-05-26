2e419c0de8b7edb110f33a5e5b998b9a951706ce
christopherstief <christopher.stief@student.uibk.ac.at>
2024-05-26 16:33:39 +0200

    changed name AccessPointDto

--- tempera-server/src/main/java/at/qe/skeleton/rest/raspberry/dtos/AccessPointAllowedDto.java
+++ tempera-server/src/main/java/at/qe/skeleton/rest/raspberry/dtos/AccessPointAllowedDto.java
@@ -1,9 +1,7 @@
 package at.qe.skeleton.rest.raspberry.dtos;
 
-import at.qe.skeleton.model.TemperaStation;
-
 import java.util.List;
 import java.util.UUID;
 
-public record AccessPointDto(
+public record AccessPointAllowedDto(
     UUID id, boolean access_point_allowed, List<String> stations_allowed) {}
