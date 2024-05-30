f6f24a8f787f40c00650985666605764b1c7ced3
Andreas Enk <andreas.enk@student.uibk.ac.at>
2024-05-31 00:07:38 +0200

    final renaming and apiClient generation

--- tempera-server/src/main/java/at/qe/skeleton/rest/frontend/controllers/AlertController.java
+++ tempera-server/src/main/java/at/qe/skeleton/rest/frontend/controllers/AlertController.java
@@ -13,25 +13,25 @@ import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
 @RestController
-@RequestMapping(value = "/api/warnings", produces = "application/json")
+@RequestMapping(value = "/api/alerts", produces = "application/json")
 public class AlertController {
   private static final Logger logger = LoggerFactory.getLogger(AlertController.class);
 
   @GetMapping("/all")
   @PreAuthorize("hasAnyAuthority('ADMIN', 'MANAGER', 'GROUPLEAD', 'EMPLOYEE')")
-  public ResponseEntity<List<AlertDto>> getWarnings() {
-    logger.info("getAllWarnings called");
+  public ResponseEntity<List<AlertDto>> getAlerts() {
+    logger.info("getAlerts called");
     return ResponseEntity.ok(
-        List.of(
-            new AlertDto("1", "Warning 1", "2021-08-01T12:00:00", "2021-08-01T13:00:00", AlertSeverity.WARNING, SensorType.HUMIDITY),
-            new AlertDto("2", "Warning 2", "2021-08-01T10:00:00", "2021-08-01T10:30:00", AlertSeverity.WARNING, SensorType.IRRADIANCE),
-            new AlertDto("3", "Warning 3", "2021-08-02T12:00:00", "2021-08-02T14:00:00", AlertSeverity.WARNING, SensorType.TEMPERATURE)));
+            List.of(
+                    new AlertDto("1", "Warning 1", "2021-08-01T12:00:00", "2021-08-01T13:00:00", AlertSeverity.WARNING, SensorType.HUMIDITY),
+                    new AlertDto("2", "Warning 2", "2021-08-01T10:00:00", "2021-08-01T10:30:00", AlertSeverity.WARNING, SensorType.IRRADIANCE),
+                    new AlertDto("3", "Warning 3", "2021-08-02T12:00:00", "2021-08-02T14:00:00", AlertSeverity.WARNING, SensorType.TEMPERATURE)));
   }
 
   @DeleteMapping("/delete/{id}")
   @PreAuthorize("hasAnyAuthority('ADMIN', 'MANAGER', 'GROUPLEAD', 'EMPLOYEE')")
-  public ResponseEntity<String> deleteWarning(@PathVariable String id) {
-    logger.info("deleteWarning called with id: {} ", id);
-    return ResponseEntity.ok("Warning deleted");
+  public ResponseEntity<String> deleteAlert(@PathVariable String id) {
+    logger.info("deleteAlert called with id: {} ", id);
+    return ResponseEntity.ok("Alert deleted");
   }
 }
--- tempera-server/webapp/src/app/_stores/alert-store.service.ts
+++ tempera-server/webapp/src/app/_stores/alert-store.service.ts
@@ -1,26 +1,25 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, distinctUntilChanged } from 'rxjs';
-import { WarningControllerService, WarningDto } from '../../api';
+import { AlertControllerService, AlertDto } from '../../api';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AlertStoreService {
-  private alerts$ = new BehaviorSubject<WarningDto[]>([]);
+  private alerts$ = new BehaviorSubject<AlertDto[]>([]);
 
 
-  constructor(private warningControllerService: WarningControllerService) {
+  constructor(private alertControllerService: AlertControllerService) {
   }
 
   getAlerts() {
-    // todo: remove logging
     return this.alerts$.pipe(distinctUntilChanged());
   }
 
   removeAlert(id: string) {
     // remove directly from the store and then call the api to increase responsiveness
     this.alerts$.next([...this.alerts$.value.filter(w => w.id !== id)]);
-    this.warningControllerService.deleteWarning(id).subscribe({
+    this.alertControllerService.deleteAlert(id).subscribe({
       error: err => {
         // todo: handle /display error
         console.log(err);
@@ -35,7 +34,7 @@ export class AlertStoreService {
   }
 
   private fetchAlerts() {
-    this.warningControllerService.getWarnings().subscribe({
+    this.alertControllerService.getAlerts().subscribe({
       next: res => {
         this.alerts$.next(res);
       },
