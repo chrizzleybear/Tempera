0d4e389bb3e22b0b505e0f61f2b550bd9fa4a209
Leonardo Pedri <leonardopedri1.0@gmail.com>
2024-06-13 18:39:58 +0200

    implement time filtering via input boxes

--- tempera-server/webapp/src/app/climateCharts/temperature-co2-chart/temperature-co2-chart.component.ts
+++ tempera-server/webapp/src/app/climateCharts/temperature-co2-chart/temperature-co2-chart.component.ts
@@ -2,9 +2,18 @@ import {Component, OnInit} from "@angular/core";
 import {ChartModule} from "primeng/chart";
 import {ClimateDataControllerService, ClimateMeasurementDto, Sensor} from "../../../api";
 import {MessageService} from "primeng/api";
+import {MessagesModule} from "primeng/messages";
 import {MessageModule} from "primeng/message";
+import {FormsModule} from "@angular/forms";
+import {InputTextModule} from "primeng/inputtext";
+import {ToastModule} from "primeng/toast";
+import {isNumber} from "chart.js/helpers";
+import {InputNumberModule} from "primeng/inputnumber";
+import {PanelModule} from "primeng/panel";
+import {DropdownModule} from "primeng/dropdown";
 
 
+type TimeUnit = "SECONDS" | "MINUTES" | "HOURS" | "DAYS" | "WEEKS" | "MONTHS" | "YEARS"
 const sensorTypes: Sensor.SensorTypeEnum[] = ["TEMPERATURE", "NMVOC"];
 
 @Component({
@@ -13,29 +22,33 @@ const sensorTypes: Sensor.SensorTypeEnum[] = ["TEMPERATURE", "NMVOC"];
   imports: [
     ChartModule,
     MessageModule,
+    MessagesModule,
+    FormsModule,
+    InputTextModule,
+    ToastModule,
+    InputNumberModule,
+    PanelModule,
+    DropdownModule,
   ],
   templateUrl: './temperature-co2-chart.component.html',
   styleUrl: './temperature-co2-chart.component.css'
 })
 export class TemperatureCo2ChartComponent implements OnInit {
 
-  private intervalId: any;
-
   public timeout: number = 0.5;  // chart refresh rate in minutes
-
-  public chartData: any;
-
-  public chartOptions: any;
-
+  public timeUnit: TimeUnit = "MINUTES";
+  public timeAmount: number = 1;
   public accessPointUuid: string = "123e4567-e89b-12d3-a456-426614174001";
-
   public temperaStationId: string = "tempera_station_1";
 
+  public timeUnits: TimeUnit[] = ["SECONDS", "MINUTES", "HOURS", "DAYS", "WEEKS", "MONTHS", "YEARS"];
+  private intervalId: any;
+  public chartData: any;
+  public chartOptions: any;
   public temperatureData: ClimateMeasurementDto[] | undefined = [];
-
   public co2Data: ClimateMeasurementDto[] | undefined = [];
 
-  constructor(public climateDataControllerService: ClimateDataControllerService, public messageService: MessageService) {
+  constructor(public climateDataControllerService: ClimateDataControllerService, private messageService: MessageService) {
   }
 
 
@@ -54,7 +67,7 @@ export class TemperatureCo2ChartComponent implements OnInit {
   private getMeasurements(): void {
     for (let sensorType of sensorTypes) {
       this.climateDataControllerService.getMeasurementsBySensorType(
-        this.accessPointUuid, this.temperaStationId, sensorType
+        this.accessPointUuid, this.temperaStationId, sensorType, this.timeUnit, this.timeAmount
       ).subscribe({
         next: climateDataDto => {
           if (sensorType == "TEMPERATURE") {
@@ -190,6 +203,60 @@ export class TemperatureCo2ChartComponent implements OnInit {
         }
       }
     };
+  }
+
+  private startInterval(): void {
+    this.intervalId = setInterval(() => this.getMeasurements(), this.timeout * 60 * 1_000);
+  }
+
+  // public updateTimeUnit(unit: TimeUnit): void {
+  // this.timeUnit = unit;
+  public updateTimeAmount(): void {
+    if (!isNumber(this.timeAmount)) {
+      console.log("Invalid user input for time unit.");
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Invalid input',
+        detail: `Time amount must be a number.`
+      });
+      this.timeAmount = 1;
+    } else {
+      console.log("Changed time amount from user input to " + this.timeAmount);
+      this.messageService.add({
+        severity: 'info',
+        summary: 'Time amount Updated',
+        detail: `Time amount set to ${this.timeAmount}.`
+      });
+    }
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+    this.startInterval();
+  }
+
+  // public updateTimeout(timeout: number): void {
+  //   this.timeout = timeout;
+  public updateTimeout(): void {
+    if (!isNumber(this.timeout)) {
+      console.log("Invalid user input for timeout. Reset timeout to 0.5 minutes.");
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Invalid input',
+        detail: `Timeout must be a number. Timeout reset to 0.5 minutes.`
+      });
+      this.timeout = 0.5;
+    } else {
+      console.log("Changed timeout from user input to " + this.timeout + " minutes.");
+      this.messageService.add({
+        severity: 'info',
+        summary: 'Timeout Updated',
+        detail: `Timeout set to ${this.timeout} minutes.`
+      });
+    }
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+    this.startInterval();
   }
 
   printSettings(): void {
