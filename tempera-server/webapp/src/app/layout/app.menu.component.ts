2138b6a0d748b757e8dc52ef0725fd7362567f4b
Andreas Enk <andreas.enk@student.uibk.ac.at>
2024-06-20 19:35:27 +0200

    add guard on frontend that reroutes on missing permission

--- tempera-server/webapp/src/app/layout/app.menu.component.ts
+++ tempera-server/webapp/src/app/layout/app.menu.component.ts
@@ -26,7 +26,12 @@ export class AppMenuComponent implements OnInit {
       {
         label: 'Home',
         items: [
-          { label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/'] },
+          {
+            label: 'Dashboard',
+            icon: 'pi pi-fw pi-home',
+            routerLink: ['/'],
+            visible: this.storageService.getUser()?.roles.includes(RolesEnum.Employee),
+          },
         ],
       },
       {
@@ -79,7 +84,7 @@ export class AppMenuComponent implements OnInit {
             label: 'Climate Chart',
             icon: 'pi pi-fw pi-cloud',
             routerLink: ['/climateChart'],
-            visible: this.storageService.getUser()?.roles.includes('ADMIN')
+            visible: this.storageService.getUser()?.roles.includes('ADMIN'),
           },
           {
             label: 'Access Points',
@@ -99,16 +104,17 @@ export class AppMenuComponent implements OnInit {
             routerLink: ['/audit-logs'],
             visible: this.storageService.getUser()?.roles.includes('ADMIN'),
           },
-          { label: 'Tips',
+          {
+            label: 'Tips',
             icon: 'pi pi-fw pi-info-circle',
             routerLink: ['/tips'],
-            visible: this.storageService.getUser()?.roles.includes('ADMIN')
+            visible: this.storageService.getUser()?.roles.includes('ADMIN'),
           },
           {
             label: 'Climate Table',
             icon: 'pi pi-fw pi-table',
-            routerLink: ['/climateTable']
-          }
+            routerLink: ['/climateTable'],
+          },
         ],
       },
     ];
